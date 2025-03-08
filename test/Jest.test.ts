a9395f0a20d487318a40dd35368d80c8b1e53623
sanderkooger <sander@thisisfashion.tv>
2025-03-08 09:29:07 +0100

    docs: jest installed and created a test for jest itself

--- test/Jest.test.ts
+++ test/Jest.test.ts
@@ -1,12 +1,8 @@
 import { sum } from '../src/utils'
 import { describe, expect, test } from '@jest/globals'
 
-describe('sum module', () => {
+describe('JEST Working', () => {
   test('adds 1 + 2 to equal 3 (PASS)', () => {
     expect(sum(1, 2)).toBe(3)
-  })
-
-  test('adds 1 + 1 to equal 3 (FAIL)', () => {
-    expect(sum(1, 1)).toBe(3)
   })
 })
